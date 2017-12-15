a21a1fe661f70cd0c9f636bba83d570a8c907ea1
chencheng <291552579@qq.com>
2017-12-15 11:08:54 +0800

    update user changeinfo views

--- app/user/views.py
+++ app/user/views.py
@@ -1,10 +1,8 @@
 from flask import render_template, Blueprint, request, flash, redirect, url_for
 from flask_login import login_required, current_user, login_user
 from .forms import ChangeinfoForm
-from app.auth.models import User
 from app.mail import send_mail
 from app.utils import flash_errors
-
 blueprint = Blueprint('users', __name__, url_prefix='/users')
 
 
@@ -20,18 +18,17 @@ def change_info():
     form = ChangeinfoForm()
     if request.method == 'POST':
         if form.validate_on_submit():
-            user = User.update(username=form.username.data,
-                               email=form.email.data,
-                               password=form.new_password,
-                               active=True)
-            if current_user.email != form.email.data:
-                token = user.generate_confirmation_token()
-                send_mail(user.email, 'Confirm Your Account',
-                          'auth/confirm', user=user, token=token)
+            old_email = current_user.email
+            current_user.update(username=form.username.data,
+                                email=form.email.data,
+                                passeword=form.new_password.data)
+            if form.email.data != old_email:
+                token = current_user.generate_confirmation_token()
+                send_mail(form.email.data, 'Confirm Your Account',
+                          'auth/confirm', user=current_user, token=token)
                 flash('Seems you change your email. A confirmation email has been sent to you by email. \
                         note: open verify email better on your pc browser', 'success')
             flash('hi {}, Already update your infomation.'.format(form.username.data), 'success')
-            login_user(user)
             return redirect(url_for('users.members'))
         else:
             flash_errors(form)
